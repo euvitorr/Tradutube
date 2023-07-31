78834ea22a2a8c52a27a1bc82a8ff4c158c4d5c1
euvitorr <vitor.r.rodrigues@hotmail.com>
2023-07-30 21:16:04 -0300

    Adicionar tradução para mais idiomas

--- tradutube.py
+++ tradutube.py
@@ -1,14 +1,18 @@
-# Importe apenas as bibliotecas necessárias para esta etapa
+import subprocess
+import os
 from translate import Translator
+
+# Idiomas mais falados na internet (em ordem de uso)
+languages = ['en', 'zh-cn', 'es', 'ja', 'hi', 'ar', 'pt', 'bn', 'ru', 'de', 'jv', 'id', 'sw', 'fr', 'ur', 'tr', 'it',  'ko', 'th']
 
 # Pede ao usuário que insira uma frase para ser traduzida
 phrase = input("Insira a frase para ser traduzida: ")
 
-# Idioma de destino para a tradução
-target_language = 'en'
+# Faz a tradução da frase para cada um dos idiomas
+for language in languages:
+    # Faz a tradução
+    translator = Translator(to_lang=language)
+    translation = translator.translate(phrase)
 
-# Faz a tradução da frase
-translator = Translator(to_lang=target_language)
-translation = translator.translate(phrase)
-
-print(f"Frase traduzida para {target_language}: {translation}")
+    # Exibe a frase traduzida
+    print(f"Frase traduzida para {language}: {translation}")
